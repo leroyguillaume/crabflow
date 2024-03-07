e5b6b69e8fbc49d2c366de83b75787a15d031938
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2024-03-07 23:11:56 +0100

    feat: add image builder

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,1 +1,18 @@
+use std::io::stderr;
 
+use tracing::{subscriber::set_global_default, Level};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
+pub fn init_tracing() {
+    let env_filter = EnvFilter::builder()
+        .with_env_var("LOG_FILTER")
+        .with_default_directive(Level::INFO.into())
+        .from_env_lossy();
+    let layer = tracing_subscriber::fmt::layer()
+        .compact()
+        .with_writer(stderr);
+    let registry = Registry::default().with(env_filter).with(layer);
+    if let Err(err) = set_global_default(registry) {
+        eprintln!("failed to initialize logging: {err}");
+    }
+}
---
+++ examples/src/bin/hello-guillaume.rs
@@ -0,0 +1,8 @@
+extern crate crabflow_examples;
+
+use crabflow_examples::hello;
+
+fn main() {
+    let hello = hello("Guillaume");
+    println!("{hello}");
+}
