fd5839710e90be80783fee8052368d5145a9eafe
Guillaume Leroy <pro.guillaume.leroy@gmail.com>
2024-03-27 19:04:50 +0100

    chore(recreate-db): add opt to run migrator

--- sh/recreate-db.sh
+++ sh/recreate-db.sh
@@ -6,6 +6,17 @@ if [ -z "$DATABASE_URL" ]; then
     export DATABASE_URL=postgres://crabflow:crabflow@127.0.0.1/crabflow
 fi
 
+while getopts ":m" opt; do
+    case $opt in
+        m)
+            migrate="true"
+            ;;
+    esac
+done
+
 sqlx database drop -yD $DATABASE_URL
 sqlx database create -D $DATABASE_URL
-./sh/run.sh migrator
+
+if [ "$migrate" = "true" ]; then
+    ./sh/run.sh migrator
+fi
